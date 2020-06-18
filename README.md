3d4f99a4e7ad035c492c8248a587b85d6ed3a550
Marc Peejay Viernes <quantum.01.official@gmail.com>
2020-06-18 11:29:20 +0800

    Implement The Ask User Input Fucntionality
    > Ask user for inputs and parse it into a more understandable way.
    > Modify the README.md file by adding developer's information.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # unorthodox-calculator
 A simple calculator that uses the PyAutoGUI python module. This app will interact with an online calculator located at [https://www.online-calculator.com/full-screen-calculator/].
+
+# Contact Information
+Developer's Code Name: Qubits
+Location: Philippines
+Email: quantum.01.official@gmail.com
--- main.py
+++ main.py
@@ -6,12 +6,30 @@ import time
 cursor_position_log = []
 duration = 0.5
 
+user_input = ""
+calculation_instructions = []
+
 # Test variables
-key_names = ["zero.png", "one.png", "two.png", "three.png", "four.png", 
+key_file_names = ["zero.png", "one.png", "two.png", "three.png", "four.png", 
             "five.png", "six.png", "seven.png", "eight.png", "nine.png",
             "plus.png", "minus.png", "times.png", "divide.png", "equal.png", 
             "negate.png"]
 
+
+# Main functions
+def ask_mathematical_expression():
+    global user_input
+
+    user_input = pyautogui.prompt(text = "Compute: ", 
+                            title = "Enter Mathematical Expression", 
+                            default = "2+7")
+
+def parse_user_input(user_input):
+    global calculation_instructions
+
+    for character in user_input:
+        if character != " ":
+            calculation_instructions.append(character)
 
 # Utility functions
 def log_cursor_location():
@@ -37,14 +55,17 @@ def get_centered_key_location(key_location):
     return pyautogui.center(key_location)
 
 # Test functions
-def test_all_keys(key_names):
-    for key in key_names:
+def test_all_keys(key_file_names):
+    for key in key_file_names:
         key_location = get_key_location("images/" + key)
         centered_key_location = get_centered_key_location(key_location)
+
+        cursor_position_log.append(centered_key_location)
 
         hover(centered_key_location, duration)
 
 
+# Main code
 log_cursor_location()
 press_key("ctrl")
 
@@ -62,6 +83,14 @@ print(input)
 
 # pyautogui.screenshot("images/screenshot1.png")
 
-test_all_keys(key_names)
+# test_all_keys(key_file_names)
 
-print(cursor_position_log)
+ask_mathematical_expression()
+print(user_input)
+
+parse_user_input(user_input)
+print(calculation_instructions)
+
+pyautogui.alert(text = cursor_position_log, 
+                title = "Cursor Position Log", 
+                button = "Okay")
